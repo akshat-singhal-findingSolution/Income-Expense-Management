291226773099a1c53b8dfe2e9deda8230d1b3c69
Aksh08 <singhalakshat0109@gmail.com>
2023-04-21 15:38:46 +0530

    Income-Expense Management

---
+++ client/src/utils/apiURL.js
@@ -0,0 +1,9 @@
+// export const API_URL_USER = "http://localhost:9000/api/v1/users";
+// export const API_URL_ACC = "http://localhost:9000/api/v1/accounts";
+// export const API_URL_TRANSACTION = "http://localhost:9000/api/v1/transactions";
+// https://test-two-psi-55.vercel.app/
+
+export const API_URL_USER = "https://test-two-psi-55.vercel.app/api/v1/users";
+export const API_URL_ACC = "https://test-two-psi-55.vercel.app/api/v1/accounts";
+export const API_URL_TRANSACTION =
+  "https://test-two-psi-55.vercel.app/v1/transactions";
